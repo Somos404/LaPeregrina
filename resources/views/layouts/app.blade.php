121e5d055c38b6e710f6e1b16a3d7d26a1a1ac85
Somos 404 <somos404studio@gmail.com.ar>
2020-09-03 10:21:50 -0300

    blog, vista, ruta y controlador

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html style="border-color: rgb(255,255,255);">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Peregrina_ok</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Reenie+Beanie">
+    <link rel="stylesheet" href="/assets/css/styles.min.css">
+</head>
+
+<body>
+    
+        
+    <!-- Start: Fixed navbar starting with transparency -->
+    <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-transparency" style="font-family: Raleway, sans-serif;">
+        <div class="container">
+            <div><img src="/assets/img/peregrina.png" style="width: 162px;"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon" style="border-style: none;"></span></button></div>
+            <div
+                class="collapse navbar-collapse" id="navcol-1" style="font-family: Raleway, sans-serif;">
+                <ul class="nav navbar-nav ml-auto" style="font-family: Raleway, sans-serif;font-weight: bold;">
+                    <li class="nav-item text-uppercase" role="presentation" style="color: rgb(255,255,255);"><a class="nav-link" href="#">HOME</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-uppercase" href="#">NOSOTROS</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-uppercase" href="#">PRODUCTO</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-uppercase" href="/blog">blog</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-uppercase" href="#">contacto</a></li>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link text-uppercase" data-toggle="dropdown" aria-expanded="false" href="#">Dropdown </a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                    </li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+
+    @yield('content')
+
+
+    <div class="footer-clean">
+    <footer>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Services</h3>
+                    <ul>
+                        <li><a href="#">Web design</a></li>
+                        <li><a href="#">Development</a></li>
+                        <li><a href="#">Hosting</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">Company</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Legacy</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Careers</h3>
+                    <ul>
+                        <li><a href="#">Job openings</a></li>
+                        <li><a href="#">Employee success</a></li>
+                        <li><a href="#">Benefits</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                    <p class="copyright">Company Name © 2017</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+</div>
+ </html>
